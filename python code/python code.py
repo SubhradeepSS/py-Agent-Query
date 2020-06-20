e36a2ada5e1fe8e0ffb3e782dae43069c82f8238
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-06-20 23:16:49 +0530

    feat: initial decisions based on database

--- python code/python code.py
+++ python code/python code.py
@@ -1,7 +1,7 @@
 from random import randint
 from operator import attrgetter
 import pickle
-
+import sys
 
 class Agent:
     def __init__(self, roles, id, status=False, available_since=None):
@@ -27,27 +27,23 @@ try:
     pickle_file = open("agents.pickle","rb")
     Agent_List = pickle.load(pickle_file)
     pickle_file.close()
-    print('Following Agents are available:-\n')
+    print('\nFollowing Agents are available:-\n')
     for a in Agent_List:
         print(f"ID: {a.id}\nStatus: {a.status}\nRoles: {a.roles}\nAvailable since: {a.available_since}\n")
         ID_list.append(a.id)
     print()
 except:
-    print('No Agents present in Database')
+    print("\nNo Agents present in Database.Please create agents first to check available agents for issues\n")
     Agent_List = []
-    print()
 
 
 def agents_input():
     n = int(input('Enter the number of agents to create: '))
     print()
 
-    print('Enter the details of the agents as stated')
-    print()
+    print('Enter the details of the agents as stated\n')
 
     for i in range(n):
-        print(f'Enter details of Agent')
-
         id = int(input('Enter the ID of the agent you want to create: '))
         while id in ID_list:
             id = int(input('This ID is already present.Please enter another ID: '))
@@ -81,7 +77,7 @@ def func(roles, mode):
         if f == 0:
             agents.append(agent)
 
-    if len(agents) == 0:
+    if not agents:
         return None
 
     if mode == 1:
@@ -103,8 +99,7 @@ def issues_input():
     no_issues = int(input('Enter the number of issue queries: '))
     print()
 
-    print('Enter the details of the issues as stated')
-    print()
+    print('Enter the details of the issues as stated\n')
 
     for i in range(no_issues):
         print(f"Enter details of Issue {i+1}")
@@ -112,34 +107,42 @@ def issues_input():
         roles = list(input('Enter the roles(comma separated) with which the issue is dealing: ').split(','))
         mode = int(input('Enter the mode of issue distribution(1 for available, 2 for least_busy, 3 for random): '))
         
-        print('Agents for the issue:', end=' ')
+        print('Agent(s) for the issue:', end=' ')
 
         if func(roles,mode) is None:
-            print('No agents available for the issue')
+            print('No agents available for the issue\n')
             continue
 
         if mode == 1:
             for i in func(roles,mode):
                 print(i.id, end=' ')
-                print()
+            print()
         else:
             print(func(roles,mode))
         
         print()
 
 
-new_agent = input('Do you want to create new agents[y for yes/n for no]? ')
-if new_agent == 'y':
-    agents_input()
-
-if Agent_List:
-    pickle_file = open("agents.pickle","wb")
-    pickle.dump(Agent_List,pickle_file)
-    pickle_file.close()
-
-new_issue = input('Do you want to check for issues[y for yes/n for no]? ')
-if new_issue == 'y':
-    if not Agent_List:
-        print('Please create Agents first\n')
+def main():
+    new_agent = input('Do you want to create new agents[y for yes/n for no]? ')
+    print()
+    if new_agent == 'y':
         agents_input()
-    issues_input()
+    else:
+        if not Agent_List:
+            ip = input('\nDo you really want to quit creating agents, as currently database does not contain any agent and issue query is not possible for an empty database of agents?[y for yes/n for no] ')
+            print()
+            if ip == 'n':
+                agents_input()
+
+    if Agent_List:
+        pickle_file = open("agents.pickle","wb")
+        pickle.dump(Agent_List,pickle_file)
+        pickle_file.close()
+
+        new_issue = input('\nDo you want to check available agents for issues[y for yes/n for no]? ')
+        print()
+        if new_issue == 'y':
+            issues_input()
+
+main()
